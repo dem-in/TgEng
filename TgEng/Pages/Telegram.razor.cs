7127075063cd8497b507dc13aa4f3d0e4e208e73
Dmitry <Dmitry@DESKTOP-QJ7F8VO>
2023-08-12 03:04:18 +0300

    Add Teleragm version parameter

--- TgEng/Pages/Telegram.razor.cs
+++ TgEng/Pages/Telegram.razor.cs
@@ -1,5 +1,8 @@
-﻿namespace TgEng.Pages {
-    public partial class Telegram {
+﻿using Microsoft.AspNetCore.Components;
 
+namespace TgEng.Pages {
+    public partial class Telegram {
+        [Parameter]
+        public string Version { get; set; }
     }
 }
