72b3c9c1b03e3e9ee0a6c71e2b866b7a589a3053
demindmytry <demindmytry@kodeks.ru>
2023-08-15 17:37:26 +0300

    Added telegram script calling functionality

--- TgEng/Pages/Telegram.razor.cs
+++ TgEng/Pages/Telegram.razor.cs
@@ -1,8 +1,20 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace TgEng.Pages {
     public partial class Telegram {
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+
+        private IJSObjectReference _jsModule;
+
         [Parameter]
-        public string Version { get; set; }
+        public string BotApiVersion { get; set; }
+
+        protected override async Task OnInitializedAsync() {
+            _jsModule = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./scripts/script.js");
+
+            BotApiVersion = await _jsModule.InvokeAsync<string>("GetTelegramBotApiVersion");
+        }
     }
 }
