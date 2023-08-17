1fec266b40887a7536765c243bf045ce020f4c49
demindmytry <demindmytry@kodeks.ru>
2023-08-17 03:44:23 +0300

    Added Expand functional

---
+++ TgEng/App.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace TgEng {
+    public partial class App {
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+
+        private IJSObjectReference _jsModule;
+
+        protected override async Task OnInitializedAsync() {
+            _jsModule = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./scripts.js");
+
+            await _jsModule.InvokeVoidAsync("Expand");
+        }
+    }
+}
