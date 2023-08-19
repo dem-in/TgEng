8c0c74dfe91cb1835f9b73e31239dd793529d22f
Dmitry <Dmitry@DESKTOP-QJ7F8VO>
2023-08-19 22:53:27 +0300

    Added Close method

---
+++ TgEng/Shared/NavMenu.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace TgEng.Shared {
+    public partial class NavMenu {
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+        private IJSObjectReference _jsModule;
+
+        private bool collapseNavMenu = true;
+
+        private string? NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+
+        private void ToggleNavMenu() {
+            collapseNavMenu = !collapseNavMenu;
+        }
+
+        private async Task CloseAsync() {
+            await _jsModule.InvokeVoidAsync("Close");
+        }
+
+        protected override async Task OnInitializedAsync() {
+            _jsModule = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./scripts.js");
+        }
+    }
+}
