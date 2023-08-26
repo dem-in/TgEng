8147918cea89676d4cfcd910092e2d8c90b55bf9
dem-in <dimus1991@mail.ru>
2023-08-26 09:54:43 +0000

    Deploying to gh-pages from @ dem-in/TgEng@332ab68666ec36555f29065e8223b88edd9d3e55 🚀

--- scripts.js
+++ scripts.js
@@ -1,15 +1,19 @@
-export function GetWebAppVersion() {
+export function WebAppVersion() {
     return window.Telegram.WebApp.version;
 }
 
-export function GetTelegramBotApiVersion() {
-    return window.Telegram.WebApp.version;
+export function WebAppPlatform() {
+    return window.Telegram.WebApp.platform;
 }
 
-export function Expand() {
+export function WebAppIsExpanded() {
+    return window.Telegram.WebApp.isExpanded;
+}
+
+export function WebAppExpand() {
     window.Telegram.WebApp.expand();
 }
 
-export function Close() {
+export function WebAppClose() {
     window.Telegram.WebApp.close();
 }
