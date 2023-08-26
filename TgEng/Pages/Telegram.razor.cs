76d86930646ec0a279402af12f024d86ea077813
demindmytry <demindmytry@kodeks.ru>
2023-08-26 13:15:53 +0300

    Added Telegram WebApp property on page

--- TgEng/Pages/Telegram.razor.cs
+++ TgEng/Pages/Telegram.razor.cs
@@ -7,12 +7,20 @@ namespace TgEng.Pages {
         public ITelegramWebAppProvider TelegramWebAppProvider { get; set; }
 
         [Parameter]
-        public string BotApiVersion { get; set; }
+        public string Version { get; set; }
+
+        [Parameter]
+        public string Platform { get; set; }
+
+        [Parameter]
+        public bool IsExpand { get; set; }
 
         protected override async Task OnInitializedAsync() {
             await TelegramWebAppProvider.InitializeAsync();
 
-            BotApiVersion = await TelegramWebAppProvider.GetVersionAsync();
+            Version = await TelegramWebAppProvider.GetVersionAsync();
+            Platform = await TelegramWebAppProvider.GetPlatformAsync();
+            IsExpand = await TelegramWebAppProvider.GetIsExpandedAsync();
         }
     }
 }
