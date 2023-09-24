556dc4ad228eefe9391f6b7e4c20edb24f5366d6
demindmytry <demindmytry@kodeks.ru>
2023-09-24 20:49:57 +0300

    Added CourseCover component.

---
+++ TgEng/Components/CourseCover.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TgEng.Components {
+    /// <summary>
+    /// Обложка курса
+    /// </summary>
+    public partial class CourseCover {
+        /// <summary>
+        /// Название курса
+        /// </summary>
+        [Parameter]
+        public string Title {  get; set; }
+
+        /// <summary>
+        /// Процент завершености курса
+        /// </summary>
+        [Parameter]
+        public int ComplitionPercentage { get; set; }
+
+        /// <summary>
+        /// Путь до изображения-обложки курса
+        /// </summary>
+        [Parameter]
+        public string CoverImagePath {  get; set; }
+    }
+}
